1a96ed2576d89b9dcf49b5e5954f53f1f6782544
franciosoandrea7-gif <franciosoandrea7@gmail.com>
2026-09-08 16:13:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,7 @@ def invia_email_pdf(destinatario, allegato_path, nome_cliente):
                 msg_teco.attach(part_ex)
                 
         # Spedizione tramite server SMTP
-        server = smtplib.SMTP("://gmail.com", 587)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(email_mittente, password_mittente)
         
